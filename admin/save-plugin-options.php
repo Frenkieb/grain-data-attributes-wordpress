085f46bfb0026dfc54a203ed72f7a5dfa41c8efb
Francis Bijl <francis.bijl@opencii.nl>
2018-06-06 11:48:00 +0200

    Added checks for $_POST variables.

--- admin/save-plugin-options.php
+++ admin/save-plugin-options.php
@@ -2,13 +2,13 @@
 	/**
 	 * Saves the options.
 	 */
-	if ( $_POST['save_page_variables'] ) {
+	if ( isset( $_POST['save_page_variables'] ) ) {
 		$pageVariableOptions = unserialize( GRAIN_DATA_ATTRIBUTES_PAGE_VARIABLES_CONFIG );
 
 		$pageVariableConfiguration = array();
 		foreach( $pageVariableOptions as $key => $options ) {
 			foreach( $options as $value ) {
-				$pageVariableConfiguration[$key][$value] = isset( $_POST[$value]) ? true : false;
+				$pageVariableConfiguration[$key][$value] = isset( $_POST[$value] ) ? true : false;
 			}
 		}
 
@@ -16,11 +16,11 @@
 		update_option( 'grain_data_attributes_page_variables', $pageVariableConfiguration );
 	}
 
-	if ( $_POST['disable_page_variables'] ) {
+	if ( isset( $_POST['disable_page_variables'] ) ) {
 		update_option( 'grain_data_track_page_enabled', false );
 	}
 
-	if ( $_POST['save_gtm_id'] ) {
+	if ( isset( $_POST['save_gtm_id'] ) ) {
 		update_option( 'grain_data_gtm_id', $_POST['gtmID'] );
 	}
 ?>
